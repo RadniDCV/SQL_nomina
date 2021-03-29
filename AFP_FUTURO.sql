c2642a1f20e3587f3b3b3f75bc98e290d2bb08a6
RadniDCV <rjdcv1@gmail.com>
2021-03-29 11:13:33 -0400

    Update AFP_FUTURO.sql

--- AFP_FUTURO.sql
+++ AFP_FUTURO.sql
@@ -1,171 +1,229 @@
+CREATE FUNCTION "NATURA"."Del_iMEP"(PLANI varchar(15), PLANICODE varchar(15), Gestion varchar(6), Tipo int)
+RETURNS table 
+(
+	empId varchar(20),
+    Monto decimal(18,5)
+)
+LANGUAGE SQLSCRIPT AS
 
---Ejemplo 1
-CREATE  FUNCTION [dbo].[AFP_FUTURO](@PLA varchar(50),@GEST varchar(10), @OFI varchar(20))
-/** PREVISION  FDBV**/
-RETURNS @AFP TABLE(
-CODIGO				INT, 
-CI					VARCHAR(20),
-NOMBRE				VARCHAR(150),
-NUA					VARCHAR(30),
-FECHANAC			DATETIME,
-FECHAINIC			DATETIME,
-NOVEDAD				VARCHAR(15),
-FECHA_NOV			VARCHAR(30),
-TOTALGANADO 		DECIMAL(10,2),
-DIAST				INT,
+	 MEDIDA nvarchar (3);
+	 	
+	 	BEGIN
+	 
+	 select case when :Tipo=2 then 'MES'
+	 when :Tipo=3 then 'DIA'
+	 END into MEDIDA from dummy;
+	
 
------APORTES LABORALES---
-COTIZACION			DECIMAL(10,2), 
-COMISIONAFP			DECIMAL(10,2),
-RIESGOCOMUN			DECIMAL(10,2),
-ALABORALSOL			DECIMAL(10,2),
-ANACIONALSOL		DECIMAL(10,2),
-AFP					DECIMAL(10,2),
---***************************--
+ 				
+	RETURN
+	SELECT EMP."U_EmpID" AS empId, "EncryptPL"(
+	(CASE WHEN MED."MONTO"<>0 
+	THEN 
+		0 
+	ELSE
+	(CASE WHEN :Tipo=2
+	THEN
+	(CASE WHEN h."DIA"<0 then h."MES" -1 ELSE h."MES"
+	END)
+	WHEN :Tipo=3
+	THEN (CASE WHEN h."DIA"<0 then 30 +h."DIA" ELSE h."DIA" 
+	END)
+	ELSE 0
+	END)
+	END),EMP."U_EmpID") 
+	   AS Monto
+	FROM "PL_NATURA"."@PL_EMP" EMP 
+	inner join (
+					select 
+							i."EMPID", 
+							MONTHS_BETWEEN (i."STARTDATE", i."TERMDATE") as MES, 
+							DAYS_BETWEEN (
+											ADD_MONTHS(i."STARTDATE",
+											MONTHS_BETWEEN(i."STARTDATE", 
+											i."TERMDATE")
+											), i.TermDate) as DIA
+								from (
+										select 
+												"empID" as EMPID, 
+												(
+													CASE WHEN YEAR("startDate") < LEFT(:gestion,4) THEN 
+														LEFT(:gestion,4)||'0101' 
+														ELSE 
+														"startDate" END
+												) AS STARTDATE,
+												(
+													CASE WHEN YEAR 
+														(
+															IFNULL (
+																		"termDate",
+																		LEFT(:gestion,4) || '1231')
+																	) = 
+															LEFT(:gestion,4) THEN 
+														(CASE WHEN 
+																	MONTH(
+																			IFNULL ("termDate",LEFT(:gestion,4) || '1231')
+																			)=12 
+																	AND 
+																	DAYOFMONTH(IFNULL ("termDate",LEFT(:gestion,4) || '1231')) = 30 THEN 
+																		ADD_DAYS(IFNULL ("termDate",LEFT(:gestion,4) || '1231'),1) 
+																		ELSE 
+																	IFNULL ("termDate",LEFT(:gestion,4) || '1231') END) 
+																ELSE
+														ADD_DAYS(LEFT(:gestion,4)||'1231', 1) END) AS TERMDATE
+	from OHEM) i)H ON H."EMPID" = EMP."U_EmpID" 
+	inner join (select empID, Monto from "NATURA"."Pro_xCONC"(:PLANI,'-I-' || :MEDIDA, :gestion))MED on EMP."U_EmpID" = MED."EMPID"
+	WHERE EMP."U_Cod_PL" =:PLANI 
+	ORDER BY EMP."U_EmpID";
 
------APORTES LABORALES---
-CNS					DECIMAL(10,2),
-PROVIVIENDA			DECIMAL(10,2),
-RIESGOPROF			DECIMAL(10,2),
-APATRONALSOL		DECIMAL(10,2),
-AGUINALDO     		DECIMAL(10,2),
-INDEMINIZACION     	DECIMAL(10,2),
---***************************--
+END;
 
-JUBILADO		INTEGER,
-SUCURSAL		VARCHAR(20))
-AS
+
+
+---------------------------------
+
+
+CREATE FUNCTION "NATURA"."Del_vMEP_S"( PLANI varchar(15), PLANICODE varchar(15), Gestion varchar(6), mes int)
+RETURNS table 
+(
+	empId varchar(20),
+    Monto decimal(18,5)
+)
+LANGUAGE SQLSCRIPT AS
+mesActual varchar(2);
+anoActual varchar(4);
+mesAnterior varchar(2);
+gestionAnterior varchar(6);
+cod_concepto integer;
+planilla varchar(2);
 BEGIN
 
-declare @PLA1 AS VARCHAR(50)
-SET @PLA1= @PLA --1(SELECT U_CODIGO FROM PL_LEVCORP.DBO.[@ADPR] WHERE U_ENTIDAD='PLLA' AND U_DESCRIP=@PLA)
+anoActual := substring(:Gestion, 1, 4);
+IF :PLANI = 'IS' THEN planilla := 'PS';
+----- para determinar que concepto es(I-TG1, I-TG2 o I-TG3) segun el parametro de entrada
+END IF;
+IF (:mes = 3) THEN cod_concepto := 1;
+ELSE 
+	IF (:mes = 2) THEN cod_concepto := 2;
+	ELSE cod_concepto := 3;
+	END IF;
+----- para determinar el mes
+END IF;
+--
+IF (:mes = 3) AND :PLANI = 'IS' 
+THEN mesAnterior := '10';
+ELSE
+	 IF (:mes = 2) AND :PLANI = 'IS' 
+	 THEN mesAnterior := '11';
+		ELSE 
+			IF (:mes = 1) AND :PLANI = 'IS' 
+			THEN mesAnterior := '12';
+			END IF;
+		END IF;
+	  END IF;
 
-insert into @AFP(
-CODIGO,
-CI,
-NOMBRE,
-NUA,
-FECHANAC,
-FECHAINIC,
-NOVEDAD,
-FECHA_NOV,
-TOTALGANADO,
-DIAST,
-COTIZACION,
-COMISIONAFP,
-RIESGOCOMUN,
-ALABORALSOL,
-ANACIONALSOL,
-AFP,
-CNS,
-PROVIVIENDA,
-RIESGOPROF,
-APATRONALSOL,
-AGUINALDO,
-INDEMINIZACION,
-JUBILADO,
-SUCURSAL
-)
 
-SELECT  
-PL.CODIGO,
-PL.CI,
-PL.NOMBRE,
-PL.NUA,
-PL.FECHANAC,
-PL.FECHAINIC,
-PL.NOVEDAD,
-PL.FECHA_NOV,
-sum(isnull(PL.ColT1,0)) AS TOTALGANADO,
-sum(isnull(PL.ColI2,0)) AS DIAST,
-sum(isnull(PL.ColA1,0)) AS COTIZACION,
-sum(isnull(PL.ColA2,0)) AS COMISIONAFP,
-sum(isnull(PL.ColA3,0)) AS RIESGOCOMUN,
-sum(isnull(PL.ColA4,0)) AS ALABORALSOL,
-sum(isnull(PL.ColA5,0)) AS ANACIONALSOL,
-sum(isnull(PL.ColA6,0)) AS AFP,
-sum(isnull(PL.ColR1,0)) AS CNS,
-sum(isnull(PL.ColR2,0)) AS PROVIVIENDA,
-sum(isnull(PL.ColR3,0)) AS RIESGOPROF,
-sum(isnull(PL.ColR4,0)) AS APATRONALSOL,
-sum(isnull(PL.ColR5,0)) AS AGUINALDO,
-sum(isnull(PL.ColR6,0)) AS INDEMINIZACION,
-JUBILADO,
-Sucursal 
-
-FROM 
-
-(SELECT  
-OH.EMPID AS CODIGO,
-OH.GOVID+'  '+isnull(OH.U_extCI,'') as CI,
-ISNULL(OH.[lastName],'')+' '+ISNULL(OH.[U_ApMaterno],'')+' '+ ISNULL(OH.[firstName],'')+' '+ ISNULL(OH.[middleName],'') AS NOMBRE,
-OH.U_NUA as NUA,
-isnull(OH.[birthDate],'') as FECHANAC,
-OH.STARTDATE AS FECHAINIC,
-case when month(OH.termDate)=right(@gest,2) and year(OH.termDate)=left(@gest,4) then 'Retiro' 
-when month(OH.startDate)=right(@gest,2)and year(OH.startDate)=left(@gest,4) then 'Ingreso' end  NOVEDAD,
-case when month(OH.termDate)=right(@gest,2) and year(OH.termDate)=left(@gest,4) then Convert(varchar(10), oh.termDate, 103)
-when month(OH.startDate)=right(@gest,2)and year(OH.startDate)=left(@gest,4) then Convert(varchar(10), oh.startDate, 103) end FECHA_NOV,
-
---/*---------------------------------------T O T A L E S-------------------------------------------------------------------------*/
-CASE T.U_Cod_Conc WHEN rtrim(@PLA1)+'-T-TPC' THEN [dbo].DecryptPL(T.U_Monto,T.U_Cod_Emp) ELSE 0 END AS ColT1,    /*Total Ganado*/
---***********************************************************************************************************************--
-
---/*-------------------------------------------------S A L A R I O S-----------------------------------------------------*/
-CASE T.U_Cod_Conc WHEN rtrim(@PLA1)+'-I-TDT' THEN [dbo].DecryptPL(T.U_Monto,T.U_Cod_Emp) ELSE 0 END AS ColI2,   /*Total Dias Trabajados*/
---***********************************************************************************************************************--
-
---/*---------------------------------------A P O R T E S - L A B O R A L E S-----------------------------------------------*/
-CASE T.U_Cod_Conc WHEN rtrim(@PLA1)+'-A-BCM' THEN [dbo].DecryptPL(T.U_Monto,T.U_Cod_Emp) ELSE 0 END AS ColA1,   /*BBVA Cotización Mensual (10%)*/
-CASE T.U_Cod_Conc WHEN rtrim(@PLA1)+'-A-BLC' THEN [dbo].DecryptPL(T.U_Monto,T.U_Cod_Emp) ELSE 0 END AS ColA2,   /*BBVA Comision AFP (0,5%)*/
-CASE T.U_Cod_Conc WHEN rtrim(@PLA1)+'-A-BRC' THEN [dbo].DecryptPL(T.U_Monto,T.U_Cod_Emp) ELSE 0 END AS ColA3,   /*BBVA Riesgo Comun (1,7%)*/
-CASE T.U_Cod_Conc WHEN rtrim(@PLA1)+'-A-BLS' THEN [dbo].DecryptPL(T.U_Monto,T.U_Cod_Emp) ELSE 0 END AS ColA4,   /*BBVA Aporte Laboral Solidario (0,5%)*/
-CASE T.U_Cod_Conc WHEN rtrim(@PLA1)+'-A-BNS' THEN [dbo].DecryptPL(T.U_Monto,T.U_Cod_Emp) ELSE 0 END AS ColA5,   /*BBVA Aporte Nacional Solidario (>13,000)*/
-CASE T.U_Cod_Conc WHEN rtrim(@PLA1)+'-A-BFP' THEN [dbo].DecryptPL(T.U_Monto,T.U_Cod_Emp) ELSE 0 END AS ColA6,   /*BBVA Aporte Fondo de Pensiones*/
---***********************************************************************************************************************--
-
---/*---------------------------------------A P O R T E S - P A T R O N A L E S-----------------------------------------------*/
-CASE T.U_Cod_Conc WHEN rtrim(@PLA1)+'-R-CNS' THEN [dbo].DecryptPL(T.U_Monto,T.U_Cod_Emp) ELSE 0 END AS ColR1,    /*Caja de Salud (10%)*/
-CASE T.U_Cod_Conc WHEN rtrim(@PLA1)+'-R-FFN' THEN [dbo].DecryptPL(T.U_Monto,T.U_Cod_Emp) ELSE 0 END AS ColR2,    /*BBVA PRO VIVIENDA (2%)*/
-CASE T.U_Cod_Conc WHEN rtrim(@PLA1)+'-R-FPF' THEN [dbo].DecryptPL(T.U_Monto,T.U_Cod_Emp) ELSE 0 END AS ColR3,    /*BBVA Riesgo Profesional (1,71%)*/
-CASE T.U_Cod_Conc WHEN rtrim(@PLA1)+'-R-FSO' THEN [dbo].DecryptPL(T.U_Monto,T.U_Cod_Emp) ELSE 0 END AS ColR4,    /*BBVA Aporte Patronal Solidario (3%)*/
-CASE T.U_Cod_Conc WHEN rtrim(@PLA1)+'-R-APO' THEN [dbo].DecryptPL(T.U_Monto,T.U_Cod_Emp) ELSE 0 END AS ColR5,    /*Provisión Aguinaldo (8.33%)*/
-CASE T.U_Cod_Conc WHEN rtrim(@PLA1)+'-R-API' THEN [dbo].DecryptPL(T.U_Monto,T.U_Cod_Emp) ELSE 0 END AS ColR6,    /*Provisión Indemnizacion (8.33%)*/
---************************************************************************************************************************--
-
-OH.u_juvi AS JUBILADO,
-OB.Name AS Sucursal
-
-FROM  [PL_LEVCORP].[dbo].[@PL_TX] T 
-left join OHEM OH on T.u_COD_EMP  = OH.empid
-left join OHPS OP on OP.posID =OH.position
-left join OUDP OD on OD.Code =OH.dept
-left join OUBR OB on OB.Code =OH.branch
-WHERE T.U_GESTION=@GEST AND T.U_PLANILLA = @PLA1 
---AND OH.U_TypeEmp  like '%'+@OFI+'%'--
-AND OB.Code like '%'+@OFI+'%'
- AND OH.U_CA='FDBV'
-
-) AS PL
-
-GROUP BY 
-PL.CODIGO,
-PL.CI,
-PL.NOMBRE,
-PL.NUA,
-PL.FECHANAC,
-PL.FECHAINIC,
-PL.NOVEDAD,
-PL.FECHA_NOV,
-PL.JUBILADO,
-PL.Sucursal 
-
-order by PL.NOMBRE
+ gestionAnterior := :anoActual||:mesAnterior;
 
 RETURN
+SELECT EMP."U_EmpID" AS empId, 
+	case when GES1.Monto<>0 then 0 else 
+	 "NATURA"."EncryptPL"(TPC."MONTO" ,EMP."U_EmpID") end
+	
+	 AS Monto
+FROM "PL_NATURA"."@PL_EMP" EMP 
+	inner join (select empid, Monto from "NATURA"."Pro_xCONC"(:PLANI,'-I-TG'||CAST(:cod_concepto AS varchar(1)),:Gestion))IN_ on EMP."U_EmpID"=IN_."EMPID"
+	inner join (select empid, Monto from "NATURA"."Pro_xCONC"(:planilla,'-T-TPC',:gestionAnterior))TPC on EMP."U_EmpID"=TPC."EMPID"
+	left join (select empid, Monto from "NATURA"."Pro_xCONC"(:PLANI,'-I-ME'||case when :mes= 3 then '1' when :mes=2 then '2' when :mes=1 then '3' end,:Gestion))GES1 on EMP."U_EmpID"=GES1."EMPID"
+	--inner join (select empid, Monto from "jam_xCONC"('PM','T-TPC',:gestionAnterior))TPC on EMP."U_EmpID"=TPC."EMPID"
+WHERE EMP."U_Cod_PL" =:PLANI ORDER BY EMP."U_EmpID";
+ 
+END;
 
-END
 
-/*
-select * from [dbo].[AFP_FUTURO] ('PM','201509','1') 
-*/
+
+---------------------
+
+
+CREATE FUNCTION "NATURA"."Del_iPRP"(PLANI varchar(15), PLANICODE varchar(15), Gestion varchar(6))
+RETURNS table 
+(
+	empId varchar(20),
+    Monto decimal(18,5)
+)
+LANGUAGE SQLSCRIPT AS
+BEGIN
+ 				
+	RETURN
+	SELECT EMP."U_EmpID" AS empId, 
+	(CASE WHEN PRO."MONTO"<0 
+	THEN 
+		0 
+	ELSE
+	(CASE WHEN TG1."MONTO"<> 0 and TG2."MONTO" <> 0 
+	THEN
+		case  WHEN MES."MONTO" <=3
+		then 
+		"EncryptPL"((TG2."MONTO"+TG3."MONTO")/2,EMP."U_EmpID") 
+		else
+		"EncryptPL"((TG1."MONTO"+TG2."MONTO"+TG3."MONTO")/3,EMP."U_EmpID") 
+		end
+	ELSE 
+	(CASE WHEN TG2."MONTO" <> 0 and MES."MONTO" <=3
+	THEN
+		"EncryptPL"((TG2."MONTO"+TG3."MONTO")/2,EMP."U_EmpID") 
+	ELSE
+		0 
+	END)END)END)   AS Monto
+	FROM "PL_NATURA"."@PL_EMP" EMP 
+	inner join (select empid, Monto from "NATURA"."Pro_xCONC"(:PLANI,'-I-PRO',:gestion))PRO on EMP."U_EmpID" = PRO."EMPID"
+	inner join (select empid, Monto from "NATURA"."Pro_xCONC"(:PLANI,'-I-ME3',:gestion))TG3 on EMP."U_EmpID" = TG3."EMPID"
+	inner join (select empid, Monto from "NATURA"."Pro_xCONC"(:PLANI,'-I-ME2',:gestion))TG2 on EMP."U_EmpID" = TG2."EMPID"
+	inner join (select empid, Monto from "NATURA"."Pro_xCONC"(:PLANI,'-I-ME1',:gestion))TG1 on EMP."U_EmpID" = TG1."EMPID"
+	inner join (select empid, Monto from "NATURA"."Pro_xCONC"(:PLANI,'-I-MES',:gestion))MES on EMP."U_EmpID" = MES."EMPID"
+	--inner join "NATURA"."OHEM" OH on EMP."U_EmpID"=OH."empID"
+	WHERE EMP."U_Cod_PL" =:PLANI 
+	ORDER BY EMP."U_EmpID";
+
+END;
+
+--------------
+
+
+Create FUNCTION "NATURA"."Del_iPRI"(PLANI varchar(15), PLANICODE varchar(15), Gestion varchar(6))
+RETURNS table 
+(
+	empId nchar(20),
+    Monto decimal(19,2)
+)
+LANGUAGE SQLSCRIPT AS 
+
+BEGIN
+RETURN
+	SELECT EMP."U_EmpID" as empId, 
+			(CASE WHEN AGU.Monto>0 THEN 0 
+					ELSE 
+				(CASE WHEN ANIO.MONTO =12 THEN "EncryptPL"( (PRO.Monto), EMP."U_EmpID") --ANTIGUEDAD MAYOR A UN AÑO
+					ELSE
+				(CASE WHEN ((MES.Monto *30) + DIA.Monto)>=90  THEN "EncryptPL"( ((PRO.Monto*MES.MONTO/12) + (PRO.Monto * DIA.Monto/360)) ,EMP."U_EmpID") 
+					ELSE
+					 0 				
+END )
+END )
+END )
+as Monto
+FROM "PL_NATURA"."@PL_EMP" EMP 
+	inner join (select empid, Monto from "Pro_xCONC"(:PLANI,'-I-MES',:gestion))ANIO on EMP."U_EmpID"=ANIO.EmpId
+	inner join (select empid, Monto from "Pro_xCONC"(:PLANI,'-I-MES',:gestion))MES on EMP."U_EmpID"=MES.EmpId
+	inner join (select empid, Monto from "Pro_xCONC"(:PLANI,'-I-DIA',:gestion))DIA on EMP."U_EmpID"=DIA.EmpId
+	inner join (select empid, Monto from "Pro_xCONC"(:PLANI,'-I-PRI',:gestion))AGU on EMP."U_EmpID"=AGU.EmpId
+	inner join (select empid, Monto from "Pro_xCONC"(:PLANI,'-I-PRO',:gestion))PRO on EMP."U_EmpID"=PRO.EmpId
+	left join "NATURA"."OHEM" OH on EMP."U_EmpID"=OH."empID"
+	
+	WHERE EMP."U_Cod_PL" =:PLANI ORDER BY EMP."U_EmpID";
+	
+END;
+
+
+
+
