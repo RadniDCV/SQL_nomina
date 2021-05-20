5fe4f9f357c3c1d469da07b8b7413e9899a745d3
RadniDCV <rjdcv1@gmail.com>
2021-05-20 19:01:44 -0400

    Update AFP_FUTURO.sql

--- AFP_FUTURO.sql
+++ AFP_FUTURO.sql
@@ -1,24 +1,35 @@
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-ACM','NULL',NULL,'RM','202101','0.1');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-AG2','NULL',NULL,'RM','202101','0');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-AGU','NULL',NULL,'RM','202101','0.0833');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-ALC','NULL',NULL,'RM','202101','0.005');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-ARC','NULL',NULL,'RM','202101','0.0171');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-CNS','NULL',NULL,'RM','202101','0.1');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-FON','NULL',NULL,'RM','202101','0.02');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-IMP','NULL',NULL,'RM','202101','0.13');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-IND','NULL',NULL,'RM','202101','0.0833');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-LSO','NULL',NULL,'RM','202101','0.005');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-MFP','NULL',NULL,'RM','202101','34650');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-PAG','NULL',NULL,'RM','202101','0');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-PRI','NULL',NULL,'RM','202101','0.0833');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-PSI','NULL',NULL,'RM','202101','0');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-PSO','NULL',NULL,'RM','202101','0.005');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-RPF','NULL',NULL,'RM','202101','0.0171');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-SIA','NULL',NULL,'RM','202101','0');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-SIF','NULL',NULL,'RM','202101','0');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-SIP','NULL',NULL,'RM','202101','0');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-SIQ','NULL',NULL,'RM','202101','0');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-SMN','NULL',NULL,'RM','202101','2122');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-TDM','NULL',NULL,'RM','202101','30');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-UFM','NULL',NULL,'RM','202101','2.35857');
-insert into "PL_EDV_ERP"."@PL_EXP" values('RM-P-UFN','NULL',NULL,'RM','202101','2.35851');
+set schema "EDV_ERP";
+CREATE function "Pro_iANTR"(PLANI varchar(15), PLANICODE varchar(15), Gestion varchar(6))
+RETURNS table 
+	(
+		empId varchar(20),
+    	Monto numeric(18,5)
+		
+    )
+LANGUAGE SQLSCRIPT AS  
+PARAM numeric(18,5);
+PARAM2 numeric(18,5);
+PLANIP varchar(2);
+BEGIN
+select (SELECT CAST((SELECT IFNULL((SELECT TOP 1 CAST(EXPR."U_Expre" AS nvarchar(10)) FROM "PL_EDV_ERP"."@PL_EXP" EXPR WHERE EXPR."U_Cod_EXP" = :PLANI || '-P-SMN' AND EXPR."U_Origen" IN (SELECT MAX(EXPR2."U_Origen") FROM "PL_EDV_ERP"."@PL_EXP" EXPR2 WHERE EXPR2."U_Cod_EXP" = :PLANI || '-P-SMN')), '0') FROM DUMMY) AS numeric(18,5)) FROM DUMMY) into PARAM from dummy;
+select (SELECT CAST((SELECT IFNULL((SELECT TOP 1 CAST(EXPR."U_Expre" AS nvarchar(10)) FROM "PL_EDV_ERP"."@PL_EXP" EXPR WHERE EXPR."U_Cod_EXP" = :PLANI || '-P-TDM' AND EXPR."U_Origen" IN (SELECT MAX(EXPR2."U_Origen") FROM "PL_EDV_ERP"."@PL_EXP" EXPR2 WHERE EXPR2."U_Cod_EXP" = :PLANI || '-P-TDM')), '0') FROM DUMMY) AS numeric(18,5)) FROM DUMMY) into PARAM2 from dummy;
+if :PLANI = 'IM' then
+		PLANIP := 'PM';
+	end if;	
+return 
+	SELECT EMP."U_EmpID" as empId,
+	case when  ANT.Monto <> 0  
+	then
+		0
+	else
+		"EncryptPL"((BON.Bono)-ANT2.Monto,EMP."U_EmpID") 
+	end as Monto
+	FROM "PL_EDV_ERP"."@PL_EMP" EMP inner join 
+	--(Select empId,Monto from dbo.Pro_xCONC(@PLANI,'-I-TDT',@Gestion)) TDT on TDT.empId =EMP.U_EmpID inner join
+	(Select empId,ifnull(Monto,0) as Monto from "Pro_xCONC"(:PLANIP,'-I-ANT',:Gestion)) ANT2 on ANT2.empId =EMP."U_EmpID" inner join
+	(Select empId,ifnull(Monto,0) as Monto from "Pro_xCONC"(:PLANI,'-I-ANT',:Gestion)) ANT on ANT.empId =EMP."U_EmpID" inner join
+	(Select empId,ifnull(Monto,0) as Monto from "Pro_xCONC"(:PLANI,'-I-TDT',:Gestion)) TDT on TDT.empId =EMP."U_EmpID" left outer join
+	(select empId,ifnull(Bono,0) as Bono from "BONO2"(:PARAM, :Gestion)) BON on BON.empId =EMP."U_EmpID"
+	WHERE EMP."U_Cod_PL" =:PLANI ORDER BY EMP."U_EmpID";
+	
+END;
