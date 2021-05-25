5bddb09a3bf08f9d33eb63a8bed538ace0e7ac02
RadniDCV <rjdcv1@gmail.com>
2021-05-25 16:51:38 -0400

    Update AFP_FUTURO.sql

--- AFP_FUTURO.sql
+++ AFP_FUTURO.sql
@@ -1,35 +1,48 @@
-set schema "EDV_ERP";
-CREATE function "Pro_iANTR"(PLANI varchar(15), PLANICODE varchar(15), Gestion varchar(6))
+CREATE function "Pro_a_NS_R"(PLANI varchar(15),Gestion varchar(6), CONCEPT nvarchar(8))
 RETURNS table 
-	(
-		empId varchar(20),
-    	Monto numeric(18,5)
-		
-    )
-LANGUAGE SQLSCRIPT AS  
-PARAM numeric(18,5);
-PARAM2 numeric(18,5);
-PLANIP varchar(2);
+(
+	empId varchar(20),
+    Monto decimal(18,5)
+)
+LANGUAGE SQLSCRIPT AS 
+PLANIP varchar(15);
+Param1 decimal(18,5);
+Param2 decimal(18,5);
+AFP nvarchar(4);
+v_concept varchar(15);
+
 BEGIN
-select (SELECT CAST((SELECT IFNULL((SELECT TOP 1 CAST(EXPR."U_Expre" AS nvarchar(10)) FROM "PL_EDV_ERP"."@PL_EXP" EXPR WHERE EXPR."U_Cod_EXP" = :PLANI || '-P-SMN' AND EXPR."U_Origen" IN (SELECT MAX(EXPR2."U_Origen") FROM "PL_EDV_ERP"."@PL_EXP" EXPR2 WHERE EXPR2."U_Cod_EXP" = :PLANI || '-P-SMN')), '0') FROM DUMMY) AS numeric(18,5)) FROM DUMMY) into PARAM from dummy;
-select (SELECT CAST((SELECT IFNULL((SELECT TOP 1 CAST(EXPR."U_Expre" AS nvarchar(10)) FROM "PL_EDV_ERP"."@PL_EXP" EXPR WHERE EXPR."U_Cod_EXP" = :PLANI || '-P-TDM' AND EXPR."U_Origen" IN (SELECT MAX(EXPR2."U_Origen") FROM "PL_EDV_ERP"."@PL_EXP" EXPR2 WHERE EXPR2."U_Cod_EXP" = :PLANI || '-P-TDM')), '0') FROM DUMMY) AS numeric(18,5)) FROM DUMMY) into PARAM2 from dummy;
-if :PLANI = 'IM' then
-		PLANIP := 'PM';
-	end if;	
-return 
-	SELECT EMP."U_EmpID" as empId,
-	case when  ANT.Monto <> 0  
-	then
-		0
-	else
-		"EncryptPL"((BON.Bono)-ANT2.Monto,EMP."U_EmpID") 
-	end as Monto
-	FROM "PL_EDV_ERP"."@PL_EMP" EMP inner join 
-	--(Select empId,Monto from dbo.Pro_xCONC(@PLANI,'-I-TDT',@Gestion)) TDT on TDT.empId =EMP.U_EmpID inner join
-	(Select empId,ifnull(Monto,0) as Monto from "Pro_xCONC"(:PLANIP,'-I-ANT',:Gestion)) ANT2 on ANT2.empId =EMP."U_EmpID" inner join
-	(Select empId,ifnull(Monto,0) as Monto from "Pro_xCONC"(:PLANI,'-I-ANT',:Gestion)) ANT on ANT.empId =EMP."U_EmpID" inner join
-	(Select empId,ifnull(Monto,0) as Monto from "Pro_xCONC"(:PLANI,'-I-TDT',:Gestion)) TDT on TDT.empId =EMP."U_EmpID" left outer join
-	(select empId,ifnull(Bono,0) as Bono from "BONO2"(:PARAM, :Gestion)) BON on BON.empId =EMP."U_EmpID"
-	WHERE EMP."U_Cod_PL" =:PLANI ORDER BY EMP."U_EmpID";
-	
-END;
+select (select cast((select (ifnull((SELECT TOP 1 CAST(EXPR."U_Expre" AS nvarchar(10)) FROM "PL_LIDER_PROD"."@PL_EXP" EXPR WHERE EXPR."U_Cod_EXP" = :PLANI || '-P-SMN' AND EXPR."U_Origen" IN (SELECT MAX(EXPR2."U_Origen") FROM "PL_LIDER_PROD"."@PL_EXP" EXPR2 WHERE EXPR2."U_Cod_EXP" = :PLANI || '-P-SMN' AND expr2."U_Origen" <= :GESTION)),'0')) from dummy) as decimal(18,2)) from dummy) into Param1 from dummy;
+Param2:= :Param1 * 60;
+select (SELECT CASE WHEN :PLANI = 'RM' THEN 'PM' WHEN :PLANI = 'OR' THEN 'OM' ELSE 'XX' END FROM DUMMY) into PLANIP from dummy;
+select (SELECT substring("U_Descrip", 1, 3) FROM "PL_LIDER_PROD"."@ADPR" WHERE "U_Entidad" = 'PLConcepto' AND "U_Codigo" = :CONCEPT) into AFP from dummy;
+select SUBSTRING(:CONCEPT,3,8) into v_concept from dummy;
+
+
+
+RETURN
+
+	SELECT EMP."U_EmpID" as empId--, tpc.Monto , pnsp.Monto , @Param1 , @Param2 , @AFP , @PLANIP 
+      ,
+      (CASE when IFNULL(PNS.Monto,0)>0-- OR PNSP.Monto>0
+      then 0 else 
+      case when OHEM."U_CA"=:AFP then "LIDER_PROD"."EncryptPL"
+        (round
+                (
+                CASE    WHEN (TPC_1.Monto+TPC.Monto<13000) then 0 
+                        WHEN (TPC_1.Monto+TPC.Monto-13000)>0 then 
+						 ((TPC_1.Monto+TPC.Monto-13000)*0.01) - PNS_1.Monto else 0 END        
+                ,2)
+        , EMP."U_EmpID"
+        )
+        else 0 end end
+       ) as Monto
+FROM "PL_LIDER_PROD"."@PL_EMP" EMP 
+      inner join (select empid, Monto from "LIDER_PROD"."Pro_xCONC"(:PLANI,'-T-TPC',:GESTION))TPC on EMP."U_EmpID"=TPC.EmpId
+	  inner join (select empid, Monto from "LIDER_PROD"."Pro_xCONC"(:PLANIP,'-T-TPC',:GESTION))TPC_1 on EMP."U_EmpID"=TPC_1.EmpId
+      inner join (select empid, Monto from "LIDER_PROD"."Pro_xCONC"(:PLANI,'-A-ANS',:GESTION))PNS on EMP."U_EmpID"=PNS.EmpId
+	  inner join (select empid, Monto from "LIDER_PROD"."Pro_xCONC"(:PLANIP,'-A-ANS',:GESTION))PNS_1 on EMP."U_EmpID"=PNS_1.EmpId
+      inner join OHEM on EMP."U_EmpID"=OHEM."empID"
+      WHERE EMP."U_Cod_PL" =:PLANI
+ORDER BY EMP."U_EmpID";
+end;
