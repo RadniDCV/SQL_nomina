0496067b1d8b701d725253468d02751dd06c12b7
RadniDCV <rjdcv1@gmail.com>
2021-06-04 19:01:36 -0400

    Update AFP_FUTURO.sql

--- AFP_FUTURO.sql
+++ AFP_FUTURO.sql
@@ -1,40 +1,48 @@
-DROP function "Pro_iANTR";
-CREATE function "Pro_iANTR"(PLANI varchar(15), PLANICODE varchar(15), Gestion varchar(6))
-RETURNS table 	(		empId varchar(20),    	Monto numeric(18,5)		    )
-LANGUAGE SQLSCRIPT AS 
- PARAM numeric(18,5);
- PARAM2 numeric(18,5);
- PARAM3 numeric(18,5);
- PLANIP varchar(2);
- BEGIN
- select (SELECT CAST((SELECT IFNULL((SELECT TOP 1 CAST(EXPR."U_Expre" AS nvarchar(10)) FROM "PL_EDV_ERP"."@PL_EXP" EXPR 
- WHERE EXPR."U_Cod_EXP" = :PLANI || '-P-SMN' AND EXPR."U_Origen" IN (SELECT MAX(EXPR2."U_Origen") 
- FROM "PL_EDV_ERP"."@PL_EXP" EXPR2 WHERE EXPR2."U_Cod_EXP" = :PLANI || '-P-SMN')), '0') FROM DUMMY) AS numeric(18,5)) FROM DUMMY) into PARAM 
- from dummy;
- select (SELECT CAST((SELECT IFNULL((SELECT TOP 1 CAST(EXPR."U_Expre" AS nvarchar(10)) FROM "PL_EDV_ERP"."@PL_EXP" EXPR 
- WHERE EXPR."U_Cod_EXP" = :PLANI || '-P-TDM' AND EXPR."U_Origen" IN (SELECT MAX(EXPR2."U_Origen")
- FROM "PL_EDV_ERP"."@PL_EXP" EXPR2 WHERE EXPR2."U_Cod_EXP" = :PLANI || '-P-TDM')), '0') FROM DUMMY) AS numeric(18,5)) FROM DUMMY) into PARAM2 
- from dummy;
- select (SELECT CAST((SELECT IFNULL((SELECT TOP 1 CAST(EXPR."U_Expre" AS nvarchar(10)) FROM "PL_EDV_ERP"."@PL_EXP" EXPR
- WHERE EXPR."U_Cod_EXP" = 'PM' || '-P-SMN' AND EXPR."U_Origen" IN (SELECT MAX(EXPR2."U_Origen") FROM "PL_EDV_ERP"."@PL_EXP" EXPR2 
- WHERE EXPR2."U_Cod_EXP" = 'PM' || '-P-SMN')), '0') FROM DUMMY) AS numeric(18,5)) FROM DUMMY) into PARAM3 from dummy;
- if :PLANI = 'RM' then		PLANIP := 'PM';        end if;	
- return         
- SELECT EMP."U_EmpID" as empId,    
- case when  ANT.Monto <> 0        
- then		0     
- else	
- "EncryptPL"(		
-				((BON.Bono)/30)*TDT.Monto-((BON2.Bono)/30)*TDTP.Monto
- --ANT2.Monto		
- ,EMP."U_EmpID")         end	 as Monto        FROM "PL_EDV_ERP"."@PL_EMP" EMP        
- inner join(Select empId,ifnull(Monto,0) as Monto from "EDV_ERP"."Pro_xCONC"(:PLANIP,'-I-ANT',:Gestion)) ANT2 on ANT2.empId =EMP."U_EmpID"      
- inner join(Select empId,ifnull(Monto,0) as Monto from "EDV_ERP"."Pro_xCONC"(:PLANI,'-I-ANT',:Gestion)) ANT on ANT.empId =EMP."U_EmpID" 
- inner join(Select empId,ifnull(Monto,0) as Monto from "EDV_ERP"."Pro_xCONC"(:PLANI,'-I-TDT',:Gestion)) TDT on TDT.empId =EMP."U_EmpID"  
- inner join(Select empId,ifnull(Monto,0) as Monto from "EDV_ERP"."Pro_xCONC"(:PLANIP,'-I-TDT',:Gestion)) TDTP on TDTP.empId =EMP."U_EmpID" 
- left outer join	(select empId,ifnull(Bono,0) as Bono from "EDV_ERP"."BONO2"(:PARAM, :Gestion)) BON on BON.empId =EMP."U_EmpID"  
- left outer join	(select empId,ifnull(Bono,0) as Bono from "EDV_ERP"."BONO2"(:PARAM3, :Gestion)) BON2 on BON2.empId =EMP."U_EmpID" 
- WHERE EMP."U_Cod_PL" =:PLANI ORDER BY EMP."U_EmpID";      
+DROP FUNCTION "EDV_ERP"."Pro_vMA_";
+CREATE FUNCTION "EDV_ERP"."Pro_vMA_"( PLANI varchar(15), PLANICODE varchar(15), Gestion varchar(6), mes int)  
+RETURNS
+ table  ( 	empId nchar(15), Monto decimal(19,2) ) 
+ LANGUAGE SQLSCRIPT AS     
+ mesActual varchar(2);     
+ anoActual varchar(4);   
+ mesAnterior varchar(2);   
+ gestionAnterior varchar(6);    
+ cod_concepto integer;    
+ planilla varchar(2);     
+ anoAnterior varchar(4);
+ BEGIN  
+ select substring(:Gestion,5,6)into mesActual from dummy; select substring(:Gestion,1,4) into anoActual from dummy; 
+ select substring(:Gestion,1,4) into anoAnterior from dummy;  
+ mes := mes -1;  
+ SELECT RIGHT ('00'||TO_NVARCHAR(TO_INTEGER(:mesActual)-:mes),2)into mesAnterior from dummy;  
+ if (:mesAnterior =(-1)) 	
+ THEN 
+ mesAnterior := '11' ;  
+ anoAnterior :=    Right('0000'||TO_NVARCHAR(TO_INTEGER (:anoActual) -1),4)   ;	
+ else 	if (:mesAnterior=0) 	
+ THEN 	mesAnterior := '12' ;  
+ anoAnterior := Right('0000'||TO_NVARCHAR(TO_INTEGER (:anoActual) -1),4);	
+ end if; 
+ end if;  
+ if (:mes +1 =3) then cod_concepto:= 1;  
+ else   if (:mes +1 =2) then cod_concepto:= 2;  
+ else  if (:mes +1 =1)  then  cod_concepto:= 3;  
+ end if;  
+ end if; 
+ end if;  
+ SELECT :anoAnterior||:mesAnterior INTO gestionAnterior FROM DUMMY; 
+ RETURN 
+ SELECT EMP."U_EmpID" AS empId,   
+		case when IN_.Monto <>0 then 
+			0 
+			else 
+			"EDV_ERP"."EncryptPL"( 		TPC."MONTO"+TPCR."MONTO"   	,EMP."U_EmpID")  
+		end AS Monto 
+ FROM "PL_EDV_ERP"."@PL_EMP" EMP  
+ inner join (select empid, Monto from "EDV_ERP"."Pro_xCONC"(:PLANI,'-V-ME'||CAST(:cod_concepto AS varchar(1)),:Gestion))IN_ on EMP."U_EmpID"=IN_."EMPID"  
+ inner join (select empid, Monto from "EDV_ERP"."Pro_xCONC"(:PLANI,'-T-TPC',:gestionAnterior))TPC on EMP."U_EmpID"=TPC."EMPID"
+ inner join (select empid, Monto from "EDV_ERP"."Pro_xCONC"('RM','-T-TPC',:gestionAnterior))TPCR on EMP."U_EmpID"=TPCR."EMPID"
+ WHERE EMP."U_Cod_PL" =:PLANI 
+ ORDER BY EMP."U_EmpID";   
  END;
-
 
